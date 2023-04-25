db21f48a3e8db085d2cc80f5964c7f63180b587e
joshua <zaitochegne@gmail.com>
2023-04-24 21:27:55 -0500

    agregando archivos js con servidor express

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs= require("fs")
+const express = require("express");
+const app= express();
+
+const PORT = process.env.PORT|| 4000;
+
+app.get("/",(require,response)=>{
+    response.send("estamos vivos")
+})
+app.get("/jugadores",(require,response)=>{
+    fs.readFile(__dirname+"/jugadores.json","utf-8",(err,data)=>{
+        if(err){
+            console.error(err);
+            return response.status(500).json({error:"Error al leer el archivo"});
+        }
+        const jsonData = JSON.parse(data);
+        response.json(jsonData);
+    });
+})
+app.listen(PORT,()=>{
+    console.log("esta funcionando todo bien en el puerto "+PORT)
+
+})
+
